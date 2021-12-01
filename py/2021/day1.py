6aecd578cf1c1f64bc2085b48fdd1b844135985c
Daniil Rose <daniil.rose@member.fsf.org>
2021-11-30 23:25:11 -0500

    Update py-2021-1

--- py/2021/day1.py
+++ py/2021/day1.py
@@ -1,3 +1,6 @@
-from .. import utils
+def read_input(year: int, day: int):
+    with open(f"input/{year}/{day}", "r") as f:
+        return f.read()
 
-print(utils.read_input())
+
+print(read_input(2021, 1).split("\n"))
