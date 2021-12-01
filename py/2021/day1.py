7bb4c61dacaa307b2792bf37433520c5ed32cc45
Daniil Rose <daniil.rose@member.fsf.org>
2021-12-01 00:16:58 -0500

    Add solutions py-2021-01

--- py/2021/day1.py
+++ py/2021/day1.py
@@ -3,4 +3,22 @@ def read_input(year: int, day: int):
         return f.read()
 
 
-print(read_input(2021, 1).split("\n"))
+inputs = [int(x) for x in read_input(2021, 1).split("\n")]
+
+
+def part_one():
+    increase_counter = 0
+    for i in range(0, len(inputs) - 1):
+        if inputs[i] < inputs[i + 1]:
+            increase_counter += 1
+    print(f"Part One: {increase_counter}")
+
+
+def part_two():
+    increase_counter = 0
+    for i in range(0, len(inputs) - 3):
+        if (inputs[i] + inputs[i + 1] + inputs[i + 2]) < (
+            inputs[i + 1] + inputs[i + 2] + inputs[i + 3]
+        ):
+            increase_counter += 1
+    print(f"Part Two: {increase_counter}")
