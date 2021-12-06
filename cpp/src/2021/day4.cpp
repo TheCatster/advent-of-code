153b5206b87aff73abe86d20fcfed568bf43fe18
Daniil Rose <daniil.rose@member.fsf.org>
2021-12-06 14:15:49 -0500

    Push current progress for cpp day 4 part 1

--- cpp/src/2021/day4.cpp
+++ cpp/src/2021/day4.cpp
@@ -1,6 +1,15 @@
 #include "advent-of-code.h"
 
-int main()
-{
-    return 0;
+int main() {
+  std::vector<std::string> input_raw{split(read_input_str(2021, 4), "\n\n")};
+  std::vector<std::string> called_numbers{split(input_raw[0], ",")};
+  std::vector<std::string> boards;
+
+  for (std::string i : input_raw | std::views::drop(1)) {
+    boards.push_back(i);
+  }
+
+  std::cout << boards[0] << std::endl;
+
+  return 0;
 }
