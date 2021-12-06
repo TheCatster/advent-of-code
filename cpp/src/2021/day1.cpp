ea8b3ba279d4a91319f5ea15a8f123bb8a345b7a
Daniil Rose <daniil.rose@member.fsf.org>
2021-12-05 20:04:12 -0500

    Reformat all cpp solutions thus far

--- cpp/src/2021/day1.cpp
+++ cpp/src/2021/day1.cpp
@@ -3,41 +3,36 @@
 void part_one(std::vector<int> &int_input);
 void part_two(std::vector<int> &int_input);
 
-int main()
-{
-    std::vector<std::string> raw_input = read_input(2021, 1);
-    std::vector<int> int_input;
+int main() {
+  std::vector<std::string> raw_input{read_input(2021, 1)};
+  std::vector<int> int_input;
 
-    std::transform(raw_input.begin(), raw_input.end(), std::back_inserter(int_input),
-                   [](const std::string &str)
-                   { return std::stoi(str); });
+  std::transform(raw_input.begin(), raw_input.end(),
+                 std::back_inserter(int_input),
+                 [](const std::string &str) { return std::stoi(str); });
 
-    part_one(int_input);
-    part_two(int_input);
+  part_one(int_input);
+  part_two(int_input);
 
-    return 0;
+  return 0;
 }
 
-void part_one(std::vector<int> &int_input)
-{
-    int increase_counter = 0;
+void part_one(std::vector<int> &int_input) {
+  int increase_counter{0};
 
-    for (int i = 1; i < int_input.size(); i++)
-    {
-        increase_counter += int_input[i] > int_input[i - 1];
-    }
+  for (int i{1}; i < int_input.size(); i++) {
+    increase_counter += int_input[i] > int_input[i - 1];
+  }
 
-    std::cout << "Part One: " << increase_counter << std::endl;
+  std::cout << "Part One: " << increase_counter << std::endl;
 }
 
-void part_two(std::vector<int> &int_input)
-{
-    int increase_counter = 0;
+void part_two(std::vector<int> &int_input) {
+  int increase_counter{0};
 
-    for (int i = 3; i < int_input.size(); i++)
-    {
-        increase_counter += int_input[i] > int_input[i - 3];
-    }
+  for (int i{3}; i < int_input.size(); i++) {
+    increase_counter += int_input[i] > int_input[i - 3];
+  }
 
-    std::cout << "Part Two: " << increase_counter << std::endl;
+  std::cout << "Part Two: " << increase_counter << std::endl;
 }
--- cpp/src/2021/day3.cpp
+++ cpp/src/2021/day3.cpp
@@ -4,7 +4,7 @@ void part_one(std::vector<std::string> &raw_input);
 void part_two(std::vector<std::string> &raw_input);
 
 int main() {
-  std::vector<std::string> raw_input = read_input(2021, 3);
+  std::vector<std::string> raw_input{read_input(2021, 3)};
 
   part_one(raw_input);
   part_two(raw_input);
@@ -13,23 +13,23 @@ int main() {
 }
 
 void part_one(std::vector<std::string> &raw_input) {
-  std::vector<int> counter_vec(12, 0);
+  std::vector<int> counter_vec{12, 0};
 
-  std::string gamma_bin_str = "";
-  std::string epsilon_bin_str = "";
+  std::string gamma_bin_str{""};
+  std::string epsilon_bin_str{""};
 
   for (auto &raw_number : raw_input) {
-    for (int i = 0; i < raw_number.size(); i++) {
-      std::string raw_digit(1, raw_number[i]);
-      int digit = std::stoi(raw_digit);
+    for (int i{0}; i < raw_number.size(); i++) {
+      std::string raw_digit{1, raw_number[i]};
+      int digit{std::stoi(raw_digit)};
 
       counter_vec[i] += digit;
     }
   }
 
   for (auto &x : counter_vec) {
-    int num_ones = x;
-    int num_zeroes = raw_input.size() - num_ones;
+    int num_ones{x};
+    int num_zeroes{raw_input.size() - num_ones};
 
     if (num_ones > num_zeroes) {
       gamma_bin_str += "1";
@@ -40,8 +40,8 @@ void part_one(std::vector<std::string> &raw_input) {
     }
   }
 
-  int gamma_value = std::stoi(gamma_bin_str, 0, 2);
-  int epsilon_value = std::stoi(epsilon_bin_str, 0, 2);
+  int gamma_value{std::stoi(gamma_bin_str, 0, 2)};
+  int epsilon_value{std::stoi(epsilon_bin_str, 0, 2)};
 
   std::cout << "Part One: " << gamma_value * epsilon_value << std::endl;
 }
